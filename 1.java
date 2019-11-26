75d04ce07bf11b175b20f5a1e03ad671748d121f
SiddharthaNG447 <58068418+SiddharthaNG447@users.noreply.github.com>
2019-11-26 16:51:15 +0530

    Add files via upload

---
+++ 1.java
@@ -0,0 +1,10 @@
+ class Test
+{
+  public static void main(String[] args)
+  {
+	  int b=123;
+	  int[] a=new int[b];
+	  System.out.println("the length of the array is the   :"+a.length);
+  }
+}
+  
